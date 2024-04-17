e4dc6187c27d66c553fe02e3ebb65adea3cde42a
Etienne Millon <me@emillon.org>
2024-04-17 16:00:46 +0200

    Use 23.11

--- flake.nix
+++ flake.nix
@@ -1,9 +1,27 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        overlay = final: prev: {
+          ocamlPackages = prev.ocamlPackages.overrideScope' (ofinal: oprev: {
+            curly = oprev.curly.overrideAttrs (oldAttrs:
+              let version = "0.3.0";
+              in {
+                inherit version;
+                src = prev.fetchurl {
+                  url =
+                    "https://github.com/rgrinberg/curly/releases/download/${version}/curly-${version}.tbz";
+                  hash = "sha256-Qn/PKBNOcMt3dk2f7uJD8x0yo4RHobXSjTQck7fcXTw=";
+                };
+              });
+          });
+        };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        };
       in {
         packages.default = pkgs.ocamlPackages.buildDunePackage {
           pname = "opam-repo-ci-diff";
